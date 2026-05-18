e85169687ab81db3e1ecaa530e482fca08548666
Rasmus Ros <rasmus.ros@theca.com>
2026-05-18 15:43:44 +0200

    chore: bump kbuild to 1.1.5 and run lintDocs in CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
-    id("com.eignex.kmp") version "1.1.4"
+    id("com.eignex.kmp") version "1.1.5"
     kotlin("plugin.serialization") version "2.3.0"
 }
 
