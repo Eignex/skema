3bafa9e8c7681e596b4572013ef0b3be4f6f2244
Rasmus Ros <rasmus.ros@theca.com>
2026-05-21 21:24:37 +0200

    chore: drop deprecated macosX64 target

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ kotlin {
     wasmJs { browser(); nodejs() }
     wasmWasi { nodejs() }
     linuxX64(); linuxArm64()
-    macosX64(); macosArm64(); mingwX64()
+    macosArm64(); mingwX64()
     iosX64(); iosArm64(); iosSimulatorArm64()
 
     sourceSets {
