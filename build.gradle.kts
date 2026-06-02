c4a8c6f0966dcca6cef31ee832ce144996d6a199
Rasmus Ros <rasmus.ros@theca.com>
2026-06-02 11:52:11 +0200

    chore: bump com.eignex.kmp to 1.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
-    id("com.eignex.kmp") version "1.2.1"
+    id("com.eignex.kmp") version "1.2.2"
     kotlin("plugin.serialization") version "2.3.20"
 }
 
