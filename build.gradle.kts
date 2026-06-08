1eeb64b9c749ea16082650c86b934f73f2ca3500
Rasmus Ros <rasmus.ros@theca.com>
2026-06-08 11:27:58 +0200

    chore: update kbuild to 1.2.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
-    id("com.eignex.kmp") version "1.2.5"
+    id("com.eignex.kmp") version "1.2.6"
     kotlin("plugin.serialization") version "2.3.20"
 }
 
